be95720617cbd5fb6ee2d764bf3f43e706b54186
Peng Wang <peng.wang@outlook.com>
2017-11-24 18:46:27 +0800

    remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,10 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, AfterViewInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
-import 'rxjs/add/operator/retryWhen';
-import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mapTo';
-import 'rxjs/add/operator/delay';
-import 'rxjs/add/operator/scan';
-import 'rxjs/add/operator/startWith';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/takeWhile';
-import 'rxjs/add/operator/concat';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/observable/fromEvent';
-import 'rxjs/add/observable/zip';
-import 'rxjs/add/observable/throw';
-import 'rxjs/add/observable/range';
-import 'rxjs/add/observable/timer';
 import 'rxjs/add/observable/interval';
-import 'rxjs/add/observable/merge';
-
-export interface Post {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}
 
 export interface CountDown {
   day: number;
@@ -46,58 +22,11 @@ export class AppComponent implements AfterViewInit {
 
   readonly _MS_PER_SECOND = 1000;
   readonly _FUTURE_DATE = '2017-11-26 00:00:00';
-  @ViewChild('increment', {read: ElementRef}) increment: ElementRef;
-  @ViewChild('decrement', {read: ElementRef}) decrement: ElementRef;
-  posts$: Observable<Post[]>;
-  countDown$: Observable<CountDown>;
-  click$: Observable<number>;
 
-  constructor(private http: HttpClient) {}
+  countDown$: Observable<CountDown>;
 
   ngAfterViewInit(): void {
     this.countDown$ = this.getCounDownObservable(new Date(), new Date(this._FUTURE_DATE));
-    this.click$ = this.getCounterObservable();
-    this.posts$ = this.getPosts();
-  }
-
-  getPosts() : Observable<Post[]> {
-    const uri = `http://jsonplaceholder.typicode.com/posts1`;
-    return this.http
-      .get<Post[]>(uri)
-      .retryWhen(err => this.getRetryStrategyObservable(err))
-  }
-
-  private getCounterObservable(): Observable<number> {
-    const increment$ = Observable
-      .fromEvent(this.increment.nativeElement, 'click')
-      .do(_ => console.log('increment'))
-      .mapTo(1);
-    const decrement$ = Observable
-      .fromEvent(this.decrement.nativeElement, 'click')
-      .debounceTime(300)
-      .do(_ => console.log('decrement'))
-      .mapTo(-1);
-    return Observable.merge(increment$, decrement$)
-      .scan((acc, curr) => acc + curr)
-      .startWith(0);
-  }
-
-  private getRetryStrategyObservable(err: Observable<any>) {
-    const retryCount$ = Observable.range(1, 5);
-    const errWithRetry$ = Observable.zip(err, retryCount$, (e, i) => ({error: e, count: i}));
-    return errWithRetry$
-      .mergeMap(({error, count}) => {
-        // not found or service unavailable
-        if(error.status === 404 || error.status === 503) {
-          // 失败后不是立即重新尝试，而是等待越来越长的时间重试
-          console.log(count);
-          return count < 5 ?
-            Observable.of(error.status).delay(Math.pow(2, count) * 1000) :
-            Observable.of(error.status);
-        }
-        return Observable.throw({error: '没有进行重试', count: count});
-      })
-      .concat(Observable.throw({error: '尝试 5 次后失败'}));
   }
 
   private diffInSec = (now: Date, future: Date): number => {
